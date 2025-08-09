ba91e5173877d6c22192078b220cc2fcfe018d52
mugada-charan <mugadacharan07@gmail.com>
2025-08-09 12:17:34 +0530

    second upload of pangram

--- Pangram.py
+++ Pangram.py
@@ -1,12 +1,16 @@
 #The given sentence must contain all 26 alphabets
+import string
 sen=input("Enter a sentence: ").lower()
 unique=set()
+allalpha=set(string.ascii_lowercase)
 for ch in sen:
   if ch.isalpha():
     if ch not in unique:
       unique.add(ch)
 
+missing=allalpha-unique
+
 if len(unique)==26:
   print("Pangram")
 else:
-  print("Not a pangram")
+  print("Not a pangram missing letters are: ",",".join(sorted(missing)))
