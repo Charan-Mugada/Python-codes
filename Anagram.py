e694ca9381c246f4f203069a2b7d11acc732c35f
mugada-charan <mugadacharan07@gmail.com>
2025-08-09 12:33:04 +0530

    Anagram

---
+++ Anagram.py
@@ -0,0 +1,19 @@
+s1=input("Enter 1st word: ").lower()
+s2=input("Enter 2nd word: ").lower()
+d1={}
+d2={}
+for ch1 in s1:
+  if ch1 in d1:
+    d1[ch1]+=1
+  else:
+    d1[ch1]=1
+for ch2 in s2:
+  if ch2 in d2:
+    d2[ch2]+=1
+  else:
+    d2[ch2]=1
+
+if d1==d2:
+  print("Anagram")
+else:
+  print("Not Anagram")
